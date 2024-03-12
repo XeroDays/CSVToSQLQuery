03ede089f9c0893621972a18590ff3ced32c511f
Sayed Muhammad Idrees <sayedidrees47@gmail.com>
2024-03-12 10:50:37 +0400

    Add project files.

---
+++ CSVToQuery/Home.Designer.cs
@@ -0,0 +1,118 @@
+﻿namespace CSVToQuery
+{
+    partial class Home
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            txtFilePath = new TextBox();
+            btnBrowse = new Button();
+            btnStart = new Button();
+            dataGridWithCheckbox = new DataGridView();
+            btnConvertTable = new Button();
+            ((System.ComponentModel.ISupportInitialize)dataGridWithCheckbox).BeginInit();
+            SuspendLayout();
+            // 
+            // txtFilePath
+            // 
+            txtFilePath.Location = new Point(24, 35);
+            txtFilePath.Name = "txtFilePath";
+            txtFilePath.Size = new Size(621, 27);
+            txtFilePath.TabIndex = 0;
+            // 
+            // btnBrowse
+            // 
+            btnBrowse.Location = new Point(656, 35);
+            btnBrowse.Name = "btnBrowse";
+            btnBrowse.Size = new Size(94, 29);
+            btnBrowse.TabIndex = 1;
+            btnBrowse.Text = "Browse";
+            btnBrowse.UseVisualStyleBackColor = true;
+            btnBrowse.Click += btnBrowse_Click;
+            // 
+            // btnStart
+            // 
+            btnStart.BackColor = Color.Green;
+            btnStart.FlatStyle = FlatStyle.Flat;
+            btnStart.ForeColor = Color.White;
+            btnStart.Location = new Point(24, 86);
+            btnStart.Name = "btnStart";
+            btnStart.Size = new Size(496, 29);
+            btnStart.TabIndex = 2;
+            btnStart.Text = "(1) Read To File";
+            btnStart.UseVisualStyleBackColor = false;
+            btnStart.Click += btnStart_Click;
+            // 
+            // dataGridWithCheckbox
+            // 
+            dataGridWithCheckbox.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+            dataGridWithCheckbox.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridWithCheckbox.Location = new Point(24, 121);
+            dataGridWithCheckbox.Name = "dataGridWithCheckbox";
+            dataGridWithCheckbox.RowHeadersWidth = 51;
+            dataGridWithCheckbox.RowTemplate.Height = 29;
+            dataGridWithCheckbox.Size = new Size(992, 360);
+            dataGridWithCheckbox.TabIndex = 3;
+            // 
+            // btnConvertTable
+            // 
+            btnConvertTable.BackColor = Color.FromArgb(255, 128, 0);
+            btnConvertTable.FlatStyle = FlatStyle.Flat;
+            btnConvertTable.ForeColor = Color.White;
+            btnConvertTable.Location = new Point(526, 86);
+            btnConvertTable.Name = "btnConvertTable";
+            btnConvertTable.Size = new Size(490, 29);
+            btnConvertTable.TabIndex = 4;
+            btnConvertTable.Text = "(2) Convert Data Table";
+            btnConvertTable.UseVisualStyleBackColor = false;
+            btnConvertTable.Click += btnConvertTable_Click;
+            // 
+            // Home
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(1036, 493);
+            Controls.Add(btnConvertTable);
+            Controls.Add(dataGridWithCheckbox);
+            Controls.Add(btnStart);
+            Controls.Add(btnBrowse);
+            Controls.Add(txtFilePath);
+            Name = "Home";
+            Text = "Home";
+            ((System.ComponentModel.ISupportInitialize)dataGridWithCheckbox).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private TextBox txtFilePath;
+        private Button btnBrowse;
+        private Button btnStart;
+        private DataGridView dataGridWithCheckbox;
+        private Button btnConvertTable;
+    }
+}
---
+++ CSVToQuery/Home.cs
@@ -0,0 +1,185 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.Common;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.LinkLabel;
+
+namespace CSVToQuery
+{
+    public partial class Home : Form
+    {
+        public Home()
+        {
+            InitializeComponent();
+        }
+
+        private void btnBrowse_Click(object sender, EventArgs e)
+        {
+            //open file dialog and take path to txtFiePath.Text
+            OpenFileDialog ofd = new OpenFileDialog();
+            //also add txt in filter
+            ofd.Filter =
+                "Text Files (*.txt)|*.txt|CSV Files (*.csv)|*.csv|All Files (*.*)|*.*";
+
+            if (ofd.ShowDialog() == DialogResult.OK)
+            {
+                txtFilePath.Text = ofd.FileName;
+            }
+
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+             
+
+            //read all the text from the file
+            string[] lines = System.IO.File.ReadAllLines(txtFilePath.Text);
+            //create a new string builder
+            StringBuilder sb = new StringBuilder();
+            //loop through all the lines
+
+
+
+            //initialize datagrid with header of the csv file
+            string[] headers = lines[0].Split(',');
+            DataTable dt = new DataTable();
+            foreach (string header in headers)
+            {
+                dt.Columns.Add(header);
+                sb.Append(header);
+                sb.Append(",");
+
+            }
+             
+
+            //put datatable in datagrid
+            dataGridWithCheckbox.DataSource = dt;
+            //make it not new row addable
+            dataGridWithCheckbox.AllowUserToAddRows = false;
+
+             
+
+            lines = lines.Skip(1).ToArray();
+
+            foreach (string line in lines)
+            {
+                //split the line by comma
+                string[] parts = line.Split(',');
+                //create a new string builder
+                StringBuilder sb2 = new StringBuilder();
+
+                //loop through all the parts
+                foreach (string part in parts)
+                {
+
+                    int n;
+                    bool isNumeric = int.TryParse(part, out n);
+                    if (isNumeric)
+                    {
+                        sb2.Append(part + ",");
+                    }
+                    else
+                    {
+                        //append the part to the string builder
+                        sb2.Append("'" + part + "',");
+                    }
+
+                }
+                //remove the last comma
+                sb2.Remove(sb2.Length - 1, 1);
+                //append the string builder to the string builder 
+
+                //put the line in datagri
+                dt.Rows.Add(parts);
+                //datagrid checkbox should be seelcted
+
+               
+            }
+             
+
+        }
+
+        private void btnConvertTable_Click(object sender, EventArgs e)
+        {
+
+
+            DataTable dt = new DataTable();
+
+            // Add columns to the DataTable based on the columns in the DataGridView
+            foreach (DataGridViewColumn column in dataGridWithCheckbox.Columns)
+            {
+                dt.Columns.Add(column.HeaderText);
+            }
+
+            // Add rows to the DataTable
+            foreach (DataGridViewRow row in dataGridWithCheckbox.Rows)
+            {
+                DataRow dataRow = dt.NewRow();
+                foreach (DataGridViewCell cell in row.Cells)
+                {
+                    dataRow[cell.ColumnIndex] = cell.Value;
+                }
+                dt.Rows.Add(dataRow);
+            }
+            //convert datatable to mysql query
+
+            string fileName = System.IO.Path.GetFileNameWithoutExtension(txtFilePath.Text);
+
+            //iterate datatable rows and make mysql query for each row
+
+            StringBuilder sb2 = new StringBuilder();
+
+             
+
+
+
+            foreach (DataRow row in dt.Rows)
+            { 
+                StringBuilder sb = new StringBuilder();
+                sb.Append("INSERT INTO " + fileName + " (");
+                foreach (DataColumn column in dt.Columns)
+                {
+                    sb.Append(column.ColumnName + ",");
+                }
+                sb.Remove(sb.Length - 1, 1);
+                sb.Append(") VALUES (");
+
+                foreach (DataColumn column in dt.Columns)
+                {
+
+                    string columnValue = row[column].ToString();
+
+                    //sanitize the column value
+                    columnValue = columnValue.Replace("'", "");
+
+
+                    int n;
+                    bool isNumeric = int.TryParse(columnValue, out n);
+                    if (isNumeric)
+                    {
+                        sb.Append(columnValue + ",");
+                    }
+                    else
+                    {
+                        sb.Append("'" + columnValue + "',");
+                    }
+                }
+                sb.Remove(sb.Length - 1, 1);
+                sb.Append(");");
+                sb2.Append(sb.ToString() + "\n");
+            }
+
+            //set to clipboard
+            Clipboard.SetText(sb2.ToString()); 
+            MessageBox.Show("SQL Copied to Clipboard ");
+             
+        }
+    }
+}
